a20437b140d50dee34b5033396e4a86ec5e90260
Asmorgath <alex.amorim95@gmail.com>
2022-10-06 05:01:51 -0300

    Exercicio 'bootcamp' dio

---
+++ src/main/java/com/dio/contabanco/ContaTerminal.java
@@ -0,0 +1,27 @@
+package com.dio.contabanco;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int numeroConta;
+        String agencia, nomeCliente;
+        double saldo;
+
+
+        System.out.println("Informe seu nome: ");
+        nomeCliente = sc.nextLine();
+        System.out.println("Informe numero da agencia: ");
+        agencia = sc.nextLine();
+        System.out.println("Informe a conta: ");
+        numeroConta = sc.nextInt();
+        System.out.println("Inform o saldo inicial: ");
+        saldo = sc.nextDouble();
+
+        System.out.println("Olá "+nomeCliente+", obrigado por " +
+                "criar uma conta em nosso banco, sua agência é "+agencia+
+                ", conta "+numeroConta+" e seu saldo "+saldo+" já está disponível" +
+                " para saque");
+    }
+}
